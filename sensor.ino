88a6d527fc38b086f53cadf729d76fb800737537
fest00n <53271606+fest00n@users.noreply.github.com>
2024-03-21 18:09:20 -0600

    Create sensor.ino
    verified working for multiple dh11 sensors
    on Elegoo UNO R3

---
+++ sensor.ino
@@ -0,0 +1,43 @@
+#include "DHT.h"
+
+#define DHTPIN 2     // What digital pin we're connected to
+#define DHTTYPE DHT11   // DHT 11
+
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT11 test!");
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.println(" *C ");
+}
